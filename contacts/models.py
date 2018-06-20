a2a27cbff7f57628f9bae846e49d040f6e9027bc
Tony Cook <tcook@precisionepllc.com>
2018-06-20 00:48:58 -0700

    Populating models

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField('Contact')
+
+class Address(models.Model):
+    street_number = models.CharField('Street Number')
+    street_cardinal = models.Charfield('Street Cardinal')
+    street_class = models.CharField('Street Class')
+    city = models.CharField('City')
+    state = models.CharField('State')
+    zip_code = models.CharField('Zip Code')
+    phone_number = models.CharField('Phone Number')
--- units/models.py
+++ units/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Unit(models.Model):
+    name = models.CharField('Unit')
+    quantity_per = models.DecimalField('Quantity Per', decimal_places=5)
