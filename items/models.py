8ad7486ea6ed9ed6f8083fd4ca1721a463f5872f
Tony Cook <tcook@precisionepllc.com>
2018-06-20 12:53:32 -0700

    Registering models to admin

--- items/models.py
+++ items/models.py
@@ -9,3 +9,7 @@ class Item(models.Model):
     description = models.CharField('Description', max_length=255)
     milliliters_custom = models.DecimalField('Custom Milliliters', max_digits=10, decimal_places=5)
     unit_factor_custom = models.DecimalField('Custom Unit Factor', max_digits=10, decimal_places=5)
+    generic_item = models.ForeignKey('GenericItem', on_delete=SET_NULL, verbose_name="generic item")
+    unit = models.ForeignKey('units.Unit', on_delete=SET_NULL)
+    base_unit_custom = models.ForeignKey('units.Unit', on_delete=SET_NULL, verbose_name="custom base unit")
+    unit_factor_custom = models.DecimalField('Custom Unit Factor', max_digits=10, decimal_places=5)
--- transactions/models.py
+++ transactions/models.py
@@ -3,12 +3,16 @@ from django.db import models
 
 class Transaction(models.Model):
     date = models.DateTimeField('Date')
+    #card_used = models.ForeignKey('accounts.card_number_last4', on_delete=SET_NULL, verbose_name="card used")
     total_custom = models.DecimalField('Custom Total', max_digits=10, decimal_places=2)
     tax_total_custom = models.DecimalField('Custom Tax Total', max_digits=10, decimal_places=2)
     tip_total = models.DecimalField('Tip Total', max_digits=10, decimal_places=2)
     bags_total = models.DecimalField('Bags Total', max_digits=10, decimal_places=2)
     donation_total = models.DecimalField('Donation Total', max_digits=10, decimal_places=2)
     grand_total_custom = models.DecimalField('Custom Grand Total', max_digits=10, decimal_places=2)
+    contact_custom = models.ForeignKey('contacts.Contact', on_delete=SET_NULL, verbose_name="custom contact")
+    account_from = models.ForeignKey('accounts.Account', on_delete=SET_NULL, verbose_name="from account")
+    account_to = models.ForeignKey('accounts.Account', on_delete=SET_NULL, verbose_name="to account")
 
 class TransactionDetail(models.Model):
     quantity = models.DecimalField('Quantity', max_digits=10, decimal_places=5)
