c0d0bc3efc434359499af7e3ac18a6a9297c30fc
Tony Cook <tcook@precisionepllc.com>
2018-06-20 01:17:58 -0700

    Populating models

--- items/models.py
+++ items/models.py
@@ -7,5 +7,5 @@ class Item(models.Model):
     upc = models.CharField('UPC Code', max_length=255)
     model_number = models.CharField('Model/Item Number', max_length=255)
     description = models.CharField('Description', max_length=255)
-    milliliters_custom = models.DecimalField('Custom Milliliters', decimal_places=5)
-    unit_factor_custom = models.DecimalField('Custom Unit Factor', decimal_places=5)
+    milliliters_custom = models.DecimalField('Custom Milliliters', max_digits=10, decimal_places=5)
+    unit_factor_custom = models.DecimalField('Custom Unit Factor', max_digits=10, decimal_places=5)
--- units/models.py
+++ units/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Unit(models.Model):
     name = models.CharField('Unit', max_length=255)
-    quantity_per = models.DecimalField('Quantity Per', decimal_places=5)
+    quantity_per = models.DecimalField('Quantity Per', max_digits=10, decimal_places=5)
