44dff458699755fd48d425309824aac7931bfd6f
Tony Cook <tcook@precisionepllc.com>
2018-06-20 00:36:00 -0700

    Populating models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    name = models.CharField('Account Name')
+    account_number = models.CharField('Account Number')
+    card_number_last4 = models.CharField('Card Number (last 4 digits)')
+    
--- items/models.py
+++ items/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class GenericItem(models.Model):
+    name = models.CharField('Item')
+
+class Item(models.Model):
+    upc = models.CharField('UPC Code')
+    model_number = models.CharField('Model/Item Number')
+    description = models.CharField('Description')
+    milliliters_custom = models.DecimalField('Custom Milliliters', decimal_places=5)
+    unit_factor_custom = models.DecimalField('Custom Unit Factor', decimal_places=5)
