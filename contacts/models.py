be56ba7e3353839859e8ff7ca3ca9452ff8b5777
Tony Cook <tcook@precisionepllc.com>
2018-06-20 01:15:28 -0700

    Populating models

--- contacts/models.py
+++ contacts/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
 
 class Address(models.Model):
     street_number = models.CharField('Street Number', max_length=255)
-    street_cardinal = models.Charfield('Street Cardinal', max_length=255)
+    street_cardinal = models.CharField('Street Cardinal', max_length=255)
     street_class = models.CharField('Street Class', max_length=255)
     city = models.CharField('City', max_length=255)
     state = models.CharField('State', max_length=255)
