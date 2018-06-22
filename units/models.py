a35dbb4bf01fa1c0ad622bf6fbdc151b7c3f4016
Tony Cook <tcook@precisionepllc.com>
2018-06-22 15:18:47 -0700

    Adding model __str__

--- units/models.py
+++ units/models.py
@@ -4,6 +4,7 @@ class Unit(models.Model):
     name = models.CharField('Unit', max_length=255, blank=True)
     quantity_per = models.DecimalField('Quantity Per', max_digits=10, decimal_places=5, default=1)
     subunit = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, related_name='unit_as_subunit_set', verbose_name="subunit")
+    abbreviation = models.CharField('Abbreviation', max_length=255, blank=True)
 
     def __str__(self):
         return self.name
