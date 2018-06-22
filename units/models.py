117fc46a4e8933df11ddd304592ec829b2e6acac
Tony Cook <tcook@precisionepllc.com>
2018-06-22 15:17:15 -0700

    Adding model __str__

--- units/models.py
+++ units/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Unit(models.Model):
     name = models.CharField('Unit', max_length=255, blank=True)
     quantity_per = models.DecimalField('Quantity Per', max_digits=10, decimal_places=5, default=1)
-    subunit = models.ForeignKey('self', null=True, on_delete=models.SET_NULL, related_name='unit_as_subunit_set', verbose_name="subunit")
+    subunit = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, related_name='unit_as_subunit_set', verbose_name="subunit")
 
     def __str__(self):
         return self.name
