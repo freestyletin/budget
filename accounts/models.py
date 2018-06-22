56def2659973682131cc2f2566c6cb3a05227e2f
Tony Cook <tcook@precisionepllc.com>
2018-06-22 13:24:58 -0700

    Adding model __str__

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,6 @@ class Account(models.Model):
     name = models.CharField('Account Name', max_length=255, blank=True)
     account_number = models.CharField('Account Number', max_length=255, blank=True)
     card_number_last4 = models.CharField('Card Number (last 4 digits)', max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
