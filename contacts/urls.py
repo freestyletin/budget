3d6232882f77494712073445c233f76ac64640a7
Tony Cook <tcook@precisionepllc.com>
2018-06-24 08:30:55 -0700

    Testing unit_factor function

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'contacts'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('addresses/', views.IndexView.as_view(), name='index')
+    path('addresses/', views.AddressIndexView.as_view(), name='index')
 ]
