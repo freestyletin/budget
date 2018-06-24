bdd89309d9ed0bf7b14fd7e6cf889403012eea7c
Tony Cook <tcook@precisionepllc.com>
2018-06-24 08:29:25 -0700

    Testing unit_factor function

--- contacts/models.py
+++ contacts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField('Contact', max_length=255)
-    testfield = models.Charfield('test', max_length=255)
+    testfield = models.CharField('test', max_length=255)
 
     def __str__(self):
         return self.name
--- contacts/views.py
+++ contacts/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Contact
+from .models import Address, Contact
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
@@ -16,3 +16,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Contact.objects.order_by('name')
+
+class AddressIndexView(generic.ListView):
+    template_name = 'addresses/index.html'
+
+    def get_queryset(self):
+        return Address.objects.order_by('name')
