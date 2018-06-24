7223de609fc68a5dc3fe5be06b46ddcb43434eb7
Tony Cook <tcook@precisionepllc.com>
2018-06-24 08:46:53 -0700

    Testing unit_factor function

--- budget/urls.py
+++ budget/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('units/', include('units.urls'))
-    path('contacts/', include('contacts.urls'))
+    path('units/', include('units.urls')),
+    path('contacts/', include('contacts.urls')),
 ]
