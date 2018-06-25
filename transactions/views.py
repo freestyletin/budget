4a6a63b822fd47409e45fb5594f2f9fd32d4859c
Tony Cook <tcook@precisionepllc.com>
2018-06-25 12:10:44 -0700

    Testing unit_factor function

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Transaction, TransactionDetail
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
+
+class IndexView(generic.ListView):
+    template_name = 'transactions/index.html'
+
+    def get_queryset(self):
+        return Transaction.objects.order_by('date')
