918b96830e97a0acbb01ac551b42294e93be8a9d
Tony Cook <tcook@precisionepllc.com>
2018-06-24 20:29:47 -0700

    Testing unit_factor function

--- units/models.py
+++ units/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Unit(models.Model):
     name = models.CharField('Unit', max_length=255, blank=True)
     quantity_per = models.DecimalField('Quantity Per', max_digits=10, decimal_places=5, default=1)
-    subunit = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, related_name='unit_as_subunit_set', verbose_name="subunit")
+    subunit = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL, related_name='+', verbose_name="subunit")
     abbreviation = models.CharField('Abbreviation', max_length=255, blank=True)
 
     #unit_factor = unit.subunit.quantity_per * quantity_per
