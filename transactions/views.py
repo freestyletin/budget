cb8371d41a1a4890a89807224a991ef16969ab71
Tony Cook <tcook@precisionepllc.com>
2018-06-27 13:07:46 -0700

    Configuring modelforms

--- transactions/views.py
+++ transactions/views.py
@@ -25,7 +25,7 @@ def edit_template(request):
             model_instance = form.save(commit=False)
             model_instance.timestamp = timezone.now()
             model_instance.save()
-            return redirect('/')
+            return redirect(Transaction)
     else:
         form = TransactionForm()
         return render(request, "transactions/edit.html", {'form': form})
