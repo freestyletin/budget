f1f81ade806ef3d54fcc78615471efd3e435edd1
Tony Cook <tcook@precisionepllc.com>
2018-06-26 18:19:05 -0700

    Configuring edit transaction form

--- transactions/views.py
+++ transactions/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Transaction, TransactionDetail, TransactionForm
+from .models import Transaction, TransactionDetail
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
@@ -17,20 +17,9 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Transaction.objects.order_by('date')
 
-def edit(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = TransactionForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+class EditView(generic.ListView):
+    template_name = 'transactions/edit.html'
+    context_object_name = 'edit_transaction'
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = TransactionForm()
-
-    return render(request, 'edit.html', {'form': form})
+    def get_queryset(self):
+        return Transaction.objects.order_by('date')
