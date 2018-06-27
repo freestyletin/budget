ed0fbb20dc224023c09824ef18b7b83e287d1a0d
Tony Cook <tcook@precisionepllc.com>
2018-06-27 11:46:01 -0700

    Configuring modelforms

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'transactions'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail')
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', views.edit_template, name='edit'),
 ]
