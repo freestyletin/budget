e4d63befa905b045710647e69c9cc8133d73de4d
Tony Cook <tcook@precisionepllc.com>
2018-06-28 15:29:41 -0700

    Configuring modelforms

--- budget/settings.py
+++ budget/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'budget.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'budget/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
