e3303c65f5eff87bc4cb969bd3f67deb3db35c49
Tony Cook <tcook@precisionepllc.com>
2018-06-28 09:41:45 -0700

    Configuring modelforms

--- budget/urls.py
+++ budget/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', TemplateView.as_view(template_name="budget/index.html"), name='index'),
     path('admin/', admin.site.urls),
     path('units/', include('units.urls')),
     path('contacts/', include('contacts.urls')),
