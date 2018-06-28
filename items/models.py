6872759b165c88c4133f8dbcf71a0a1635ad87ac
Tony Cook <tcook@precisionepllc.com>
2018-06-28 12:28:53 -0700

    Configuring modelforms

--- items/models.py
+++ items/models.py
@@ -17,12 +17,11 @@ class Item(models.Model):
     upc = models.CharField('UPC Code', max_length=255, blank=True)
     model_number = models.CharField('Model/Item Number', max_length=255, blank=True)
     description = models.CharField('Description', max_length=255, blank=True)
-    milliliters_custom = models.DecimalField('Custom Milliliters', max_digits=10, decimal_places=5)
-    unit_factor_custom = models.DecimalField('Custom Unit Factor', max_digits=10, decimal_places=5)
+    milliliters_custom = models.DecimalField('Custom Milliliters', max_digits=10, decimal_places=5, blank=True)
+    unit_factor_custom = models.DecimalField('Custom Unit Factor', max_digits=10, decimal_places=5, default=1)
     generic_item = models.ForeignKey('GenericItem', blank=True, null=True, on_delete=models.SET_NULL, verbose_name="generic item", related_name='item_as_generic_item_set')
     unit = models.ForeignKey('units.Unit', null=True, on_delete=models.SET_NULL, related_name='item_as_unit_set', verbose_name="unit")
     base_unit_custom = models.ForeignKey('units.Unit', blank=True, null=True, on_delete=models.SET_NULL, related_name='item_as_base_unit_custom_set', verbose_name="custom base unit")
-    qty_test = models.DecimalField('Qty (Test)', max_digits=10, decimal_places=5, default=1)
 
 #    def unit_factor(self):
 #        return self.qty_test * self.unit.quantity_per
