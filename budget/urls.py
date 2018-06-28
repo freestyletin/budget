0956b20557d9afaf546ede761c227b36b946f0ce
Tony Cook <tcook@precisionepllc.com>
2018-06-28 09:32:50 -0700

    Configuring modelforms

--- budget/urls.py
+++ budget/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),,
     path('admin/', admin.site.urls),
     path('units/', include('units.urls')),
     path('contacts/', include('contacts.urls')),
