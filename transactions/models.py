fbc84efbcdbfaf7c96d2404eda14d38bbbec619c
Tony Cook <tcook@precisionepllc.com>
2018-06-28 10:40:44 -0700

    Configuring modelforms

--- transactions/models.py
+++ transactions/models.py
@@ -20,7 +20,7 @@ class Transaction(models.Model):
         return reverse('transactions:detail', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return self.address
+        return self.address or ""
 
 class TransactionDetail(models.Model):
     quantity = models.DecimalField('Quantity', max_digits=10, decimal_places=5)
