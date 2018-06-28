0c89eeb0c9ab640c799e70a91b846581f8bda24b
Tony Cook <tcook@precisionepllc.com>
2018-06-28 14:05:51 -0700

    Configuring modelforms

--- transactions/models.py
+++ transactions/models.py
@@ -37,7 +37,7 @@ class TransactionDetail(models.Model):
     item = models.ForeignKey('items.Item', null=True, on_delete=models.SET_NULL, related_name='transactiondetail_as_item_set')
     generic_item = models.ForeignKey('items.GenericItem', blank=True, null=True, on_delete=models.SET_NULL, related_name='transactiondetail_as_generic_item_set', verbose_name="generic item")
     base_unit_custom = models.ForeignKey('units.Unit', blank=True, null=True, on_delete=models.SET_NULL, related_name="transactiondetail_as_base_unit_custom_set", verbose_name="custom base unit")
-    unit_factor_custom = models.DecimalField('Custom Unit Factor', max_digits=10, decimal_places=5, blank=True)
+    unit_factor_custom = models.DecimalField('Custom Unit Factor', max_digits=10, decimal_places=5, blank=True, null=True)
 
     def get_absolute_url(self):
         return reverse('transactions:detail-detail', kwargs={'pk': self.pk})
