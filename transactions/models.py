b67b99afeb4842d828238dfe83abb20464e2fd96
Tony Cook <tcook@precisionepllc.com>
2018-06-28 19:24:30 -0700

    Configuring modelforms

--- transactions/models.py
+++ transactions/models.py
@@ -70,7 +70,7 @@ class TransactionDetail(models.Model):
 
     def price_extended(self):
         if self.price_final and self.quantity:
-            return Decimal(self.price_final) * self.quantity
+            return Decimal(self.price_final()) * self.quantity
         else:
             return
 
