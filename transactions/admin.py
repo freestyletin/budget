ffc1f271e8ec86abf2bf7a8e5a5d60c1c01188cd
Tony Cook <tcook@precisionepllc.com>
2019-02-10 13:53:24 -0800

    Attempting to fix TransactionDetail admin screen by not allowing Item to be null so __str__ will work

--- transactions/admin.py
+++ transactions/admin.py
@@ -5,6 +5,6 @@ from .models import Transaction, TransactionDetail
 admin.site.register(Transaction)
 admin.site.register(TransactionDetail)
 
-class TransactionDetailAdmin(admin.ModelAdmin):
-    list_display = ['item', 'quantity']
+#class TransactionDetailAdmin(admin.ModelAdmin):
+    #list_display = ['item', 'quantity']
     #readonly_fields = ('price_discounted_calculated', 'discount_calculated', 'price_regular_calculated', 'price_final', 'price_extended')
